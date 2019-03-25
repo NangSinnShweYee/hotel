3fc2e1d44210e4a596fa9c4c67cd2f39d7a4009a
michael <michaelmyers20007@gmail.com>
2019-03-25 18:07:07 +0630

    bus

--- resources/views/frontend/bus.blade.php
+++ resources/views/frontend/bus.blade.php
@@ -1,48 +1,82 @@
 @extends('frontend/master')
 @section('content')
-		<aside id="qbootstrap-hero">
-			<div class="flexslider">
-				<ul class="slides">
-			   	<li style="background-image: url(template/images/hall.jpg);" height= "100">
-			   		<div class="overlay-gradient"></div>
-			   		<div class="container-fluid">
-			   			<div class="row">
-				   			<div class="col-md-6 col-md-offset-3 slider-text">
-				   				<div class="slider-text-inner text-center">
-				   					<span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span>
-				   					<h1>Halls</h1>
-										
-				   				</div>
-				   			</div>
-				   		</div>
-			   		</div>
-			   	</li>
-			  	</ul>
-		  	</div>
-		</aside>
-<div id="qbootstrap-rooms">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 col-md-offset-3 text-center qbootstrap-heading animate-box">
-						<span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span>
-						<h2>Day Tour in Rangoon</h2>
-						
+
+
+
+<section class="pricing py-5">
+	<div class="container">
+		<div class="row">
+			<!-- Free Tier -->
+			<div class="col-lg-4">
+				<div class="card mb-5 mb-lg-0">
+					<div class="card-header">
+						<h2 class="text-center">Sight Seeing package</h2>
+					</div>
+					<div class="card-body">
+						<img src="template/images/bus1.jpg" alt="" class="card-img-top">
+						<h5 class="card-title text-muted text-uppercase text-center">Price</h5>
+						<h5 class="card-price text-center pb-4">MMK 113,409<span class="period">/person</span></h5>
+
+						<ul> 
+							<li>Full-day city sightseeing tour of Yangon</li>
+							<li>See the Yangon River, colonial buildings and Chauk Htat Gyi (reclining Buddha)</li>
+							<li>Hotel pickup and drop-off</li>
+							<li>Departure time: 8:00 AM</li>
+							<li>Arrival time: 4:00 PM</li>
+							<li>Transport by air-conditioned vehicle</li> 
+					</ul>
+					<a href="#" class="btn btn-block btn-primary text-uppercase">More Info</a>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-4">
-						<img src="{{asset('template/images/tour.jpg')}}" alt="">
-
-					</div>
-
-					<div class="col-md-4">
-						
-
-					</div>
-					<div class="col-md-4">
-						
-
-					</div>
 			</div>
+
+			<div class="col-lg-4">
+				<div class="card mb-5 mb-lg-0">
+					<div class="card-header">
+						<h2 class="text-center">Yangon Culture and Temples</h2>
+					</div>
+					<div class="card-body">
+						<img src="template/images/bus3.jpg" alt="" class="card-img-top">
+						<h5 class="card-title text-muted text-uppercase text-center">Price</h5>
+						<h5 class="card-price text-center pb-4">MMK 121,520<span class="period">/person</span></h5>
+
+						<ul> 
+							
+							<li>Visiting Shwegadon Pagoda, Sule Pagoda, Mahabandoola Park, Botataung Pagoda, and Chaukhtatgyi Temple</li>
+							<li>Hotel pickup and drop-off</li>
+							<li>Departure time: 8:00 AM</li>
+							<li>Arrival time: 4:00 PM</li>
+							<li>Transport by air-conditioned vehicle</li> 
+					</ul>
+					<a href="#" class="btn btn-block btn-primary text-uppercase">More Info</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-4">
+				<div class="card mb-5 mb-lg-0">
+					<div class="card-header">
+						<h2 class="text-center">Yangon Night Market Package</h2>
+					</div>
+					<div class="card-body">
+						<img src="template/images/bus2.jpg" alt="" class="card-img-top">
+						<h5 class="card-title text-muted text-uppercase text-center">Price</h5>
+						<h5 class="card-price text-center pb-4">MMK 49,216<span class="period">/person</span></h5>
+
+						<ul> 
+							<li>Snacks from the street market</li>
+							<li>Hotel pickup and drop-off</li>
+							<li>Departure time: 5:00 PM</li>
+							<li>Arrival time: 7:30 PM</li>
+							<li>Check out the bustling local night markets of Yangon with an experienced guide</li> 
+					</ul>
+					<a href="#" class="btn btn-block btn-primary text-uppercase">More Info</a>
+					</div>
+				</div>
+			</div>
+
 		</div>
+	</div>
+</section>
+
 @endsection
