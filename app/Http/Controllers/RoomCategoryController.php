956f3dc4d7d909dfc8da379c9dd80834766617c6
michael <michaelmyers20007@gmail.com>
2019-03-25 18:07:07 +0630

    comple room_category

--- app/Http/Controllers/RoomCategoryController.php
+++ app/Http/Controllers/RoomCategoryController.php
@@ -44,7 +44,7 @@ class RoomCategoryController extends Controller
         RoomCategory::create([
            'name'=>request('roomcategory_name')
         ]);
-        
+
        $roomcategories = RoomCategory::all();
 
         
@@ -101,6 +101,11 @@ class RoomCategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $room_category=RoomCategory::find($id);
+        foreach ($room_category->rooms as $room) {
+            $room->delete();
+        }
+        $room_category->delete();
+        return redirect('/room_categories');
     }
 }
