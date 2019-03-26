47c23a39339d64d78d83d93e0abdbcdfa0657e19
NangSinnShweYee <47998779+NangSinnShweYee@users.noreply.github.com>
2019-03-26 09:40:59 +0630

    room

--- resources/views/frontend/room.blade.php
+++ resources/views/frontend/room.blade.php
@@ -23,78 +23,10 @@
 		  	</div>
 		</aside>
 
-	<div id="qbootstrap-reservation">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12 search-wrap">
-						<form method="post" class="qbootstrap-form">
-		              	<div class="row">
-		                <div class="col-md-3">
-		                  <div class="form-group">
-		                    <label for="date">Check-in:</label>
-		                    <div class="form-field">
-		                      <i class="icon icon-calendar2"></i>
-		                      <input type="text" id="date" class="form-control date" placeholder="Check-in date">
-		                    </div>
-		                  </div>
-		                </div>
-		                <div class="col-md-3">
-		                  <div class="form-group">
-		                    <label for="date">Check-out:</label>
-		                    <div class="form-field">
-		                      <i class="icon icon-calendar2"></i>
-		                      <input type="text" id="date" class="form-control date" placeholder="Check-out date">
-		                    </div>
-		                  </div>
-		                </div>
-		                <div class="col-md-2">
-		                  <div class="form-group">
-		                    <label for="adults">Adults</label>
-		                    <div class="form-field">
-		                      <i class="icon icon-arrow-down3"></i>
-		                      <select name="people" id="people" class="form-control">
-		                        <option value="#">1</option>
-		                        <option value="#">2</option>
-		                        <option value="#">3</option>
-		                        <option value="#">4</option>
-		                        <option value="#">5+</option>
-		                      </select>
-		                    </div>
-		                  </div>
-		                </div>
-		                <div class="col-md-2">
-		                  <div class="form-group">
-		                    <label for="children">Children</label>
-		                    <div class="form-field">
-		                      <i class="icon icon-arrow-down3"></i>
-		                      <select name="people" id="people" class="form-control">
-		                        <option value="#">1</option>
-		                        <option value="#">2</option>
-		                        <option value="#">3</option>
-		                        <option value="#">4</option>
-		                        <option value="#">5+</option>
-		                      </select>
-		                    </div>
-		                  </div>
-		                </div>
-		                <div class="col-md-2">
-		                  <input type="submit" name="submit" id="submit" value="Search" class="btn btn-success btn-block">
-		                </div>
-		              </div>
-		            </form>
-					</div>
-				</div>
-			</div>
-		</div>
+	v>
 		<div id="qbootstrap-rooms">
 			<div class="container">
-				<div class="row">
-					<div class="col-md-6 col-md-offset-3 text-center qbootstrap-heading animate-box">
-						<span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span>
-						<h2>Rooms &amp; Suites</h2>
-						<p>We love to tell our successful far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-					</div>
-				</div>
+				
 				<div class="row">
 						@foreach ($rooms as $room)  
 					<div class="col-md-4 room-wrap animate-box">
