880a202722bd244f35fc12a6340a284130e31987
NangSinnShweYee <47998779+NangSinnShweYee@users.noreply.github.com>
2019-03-28 02:00:36 +0630

    hall bookingtime

--- app/Http/Controllers/HallBookingController.php
+++ app/Http/Controllers/HallBookingController.php
@@ -4,10 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\HallBooking;
-use VM\TimeOverlapCalculator\TimeOverlapCalculator;
-use VM\TimeOverlapCalculator\Entity\TimeSlot;
-use VM\TimeOverlapCalculator\Generator\TimeSlotGenerator;
-use Carbon\Carbon;
+
 
 class HallBookingController extends Controller
 {
@@ -43,51 +40,23 @@ class HallBookingController extends Controller
     {
         //
         $request->validate([
-            'check_in' => 'required|min:10',
-            'check_out' => 'required',            
+            'stat_time' => 'required|min:10',
+            'end_time' => 'required',            
         ]);
-        $check_indate = Carbon::parse( request('check_in'));
-        $check_outdate = Carbon::parse( request('check_out'));
-
-        
-        $calculator = new TimeOverlapCalculator();  
-        $baseTimeSlot = new TimeSlot(
-            $check_indate,
-            $check_outdate
-        );
-
-        $isOverlap = false;
-        
-        $bookings = HallBooking::where('hall_id','=', request('hall_id'))->get();
-        foreach ($bookings as $booking) {
-            $testin = Carbon::parse( $booking->start_time);
-            $testout = Carbon::parse( $booking->end_time);
-            $overlappingTimeSlot = new TimeSlot(
-                $testin,
-                $testout
-             );
-             $isOverlap = $calculator->isOverlap($baseTimeSlot, $overlappingTimeSlot);
-        }
-        
-
-        if($isOverlap){
-            return back()->with('overlap','The room is not available in that time period.');
-            
-        }
-        else{
+       
             // echo "not overlap";
             HallBooking::create([
                 "hall_id" => request('hall_id'),
                 "user_id" => request('user_id'),
-                "start_time" => request('check_in'),
-                "end_time" => request('check_out'),   
+                "start_time" => request('start_time'),
+                "end_time" => request('end_time'),   
     
             ]);
             return redirect('/')->with('success', 'Booking has been created');
         }
         
         
-    }
+    
     
 
     /**
