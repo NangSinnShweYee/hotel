67118789835941e5bdcf607b09d4626fa4d43d5e
NangSinnShweYee <47998779+NangSinnShweYee@users.noreply.github.com>
2019-03-28 15:04:32 +0630

    hallbookingadmin
    ma ya tay bu

--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -39,6 +39,11 @@
              <span>Halls</span></a>
      </li>
      <li class="nav-item">
+         <a class="nav-link" href="{{'/hall_bookings'}}">
+             <i class="fas fa-fw fa-tachometer-alt"></i>
+             <span>Hall Bookings</span></a>
+     </li>
+     <li class="nav-item">
             <a class="nav-link" href="{{'/bus_packages'}}">
                 <i class="fas fa-fw fa-tachometer-alt"></i>
                 <span>Bus Packages</span></a>
