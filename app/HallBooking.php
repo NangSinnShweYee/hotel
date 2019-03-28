54c682d3785e5467a2b7bf4d841bc40fc089e285
NangSinnShweYee <47998779+NangSinnShweYee@users.noreply.github.com>
2019-03-28 15:41:00 +0630

    hallbooking,room

--- app/HallBooking.php
+++ app/HallBooking.php
@@ -15,7 +15,7 @@ class HallBooking extends Model
  ];
 public function user()
     {
-        return $this->hasOne(User::class);
+        return $this->hasOne(User::class,'id','user_id');
     }
 
     public function hall()
